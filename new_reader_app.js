96f88bd520c24ef26a0057179ab474074f0471a3
Vishwash Patel <103727210+vishu3053@users.noreply.github.com>
2024-04-18 16:09:27 +0530

    successful

--- new_reader_app.js
+++ new_reader_app.js
@@ -2,9 +2,15 @@ intent('What does this app do?', 'What can I do here?',
        reply("This is a news app")
 );
 
-// callback function gonna happen after ALAN listens for 'Start a command'
+// // callback function gonna happen after ALAN listens for 'Start a command'
+// intent('Start a command', (p) => {
+//     // same as reponse 
+//     p.play('Hello, I understood your command');
+// });
+
+
+// Running a command
 intent('Start a command', (p) => {
-    // same as reponse 
-    p.play('Hello, I understood your command');
-});
+    p.play({command : 'testCommand'});
+})
 
