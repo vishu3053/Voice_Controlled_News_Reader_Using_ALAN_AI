e2ee4e1f8ec7717e28de15fc2957e0e03f45cb50
Vishwash Patel <103727210+vishu3053@users.noreply.github.com>
2024-04-18 16:56:02 +0530

    successful

--- new_reader_app.js
+++ new_reader_app.js
@@ -17,6 +17,7 @@ intent('What does this app do?', 'What can I do here?',
 
 // News API key
 const API_Key = '4faadf186f9041b3aaad120ee2e70fa5';
+let savedArticles = [];
 
 
 // Creating an API Endpoint for News by Source 
@@ -29,9 +30,25 @@ intent('Give me the news from $(source* (.*))', (p) => {
         NEWS_API_URL = `${NEWS_API_URL}&sources=${p.source.value.toLowerCase().split(" ").join('-')}`
     }
     
+// This is just a way to get the Data from the API Call using api.request 
+    
     // In ALAN, we have inbuilt functionalities for making API Call
     // we can actual data using body argument of callback function
     api.request(NEWS_API_URL, (error, response, body) => {
         const {articles} = JSON.parse(body);
+        
+        // if no articles found 
+        if(articles.length === 0) {
+            p.play('Sorry, please try searching for news from a different source');
+            return;
+        }
+        
+        // When we do get the articles, we are storing them in global variable -> savedArticles
+        savedArticles = articles;
+        
+        
+        p.play({ command: 'newHeadlines', articles });
+        p.play(`Here are the (latest|recent) ${p.source.value} news.`);
+        
     })
 });
